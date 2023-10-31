5ea6a18144132d0fb1d278ebeb7a3e40764a7c52
nasoasam <815806+nasoasam@users.noreply.github.com>
2023-11-01 01:20:45 +0900

    a

--- src/main/java/com/example/quickstart/App.java
+++ src/main/java/com/example/quickstart/App.java
@@ -9,5 +9,23 @@ public class App
     public static void main( String[] args )
     {
         System.out.println( "Hello World!!" );
+         int a;
+        System.out.println( "Hello Wor" + "ld!!" + 1 );
+        System.out.println( "Hello World!!" );
+         a = 2;
+         a++;
+         a += a(a);
+        System.out.println( "Hello World!!" );
+        System.out.println( "Hello World!!" );
+        System.out.println( "Hello World!!" );
+        System.out.println( "Hello World!!" );
+        System.out.println( "Hello World!!" );
+        System.out.println( "Hello World!!" );
+        System.out.println( "Hello World!!" );
+    }
+    public static int a(int ab) {
+    	ab = 1;
+    	return ab;
+    }
     }
 }
